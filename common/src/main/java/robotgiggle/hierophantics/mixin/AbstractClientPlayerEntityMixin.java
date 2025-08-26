7769b0a337e8ca9ec9c32846c1cc80a7409ebed8
Robotgiggle <purplemrb12@gmail.com>
2025-08-26 00:56:47 -0400

    Implement audio hallucination

---
+++ common/src/main/java/robotgiggle/hierophantics/mixin/AbstractClientPlayerEntityMixin.java
@@ -0,0 +1,59 @@
+package robotgiggle.hierophantics.mixin;
+
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.util.math.Vec3d;
+import robotgiggle.hierophantics.Hierophantics;
+import robotgiggle.hierophantics.HierophanticsClient;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Random;
+
+@Mixin(AbstractClientPlayerEntity.class)
+public class AbstractClientPlayerEntityMixin {
+    final Random RAND = new Random();
+    int villagerCooldown = 300;
+    int allayCooldown = 70;
+    
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void hallucinateAudio(CallbackInfo ci) {
+        AbstractClientPlayerEntity cPlayer = (AbstractClientPlayerEntity) (Object) this;
+        
+        // hallucinate villager nosies
+        if (villagerCooldown > 0) {
+            villagerCooldown--;
+        } else if (RAND.nextDouble() < 0.00004 * HierophanticsClient.getClientOwnedMinds()) {
+            Vec3d source = randomSpherePoint(RAND.nextDouble(3.5, 6)).add(cPlayer.getPos());
+            cPlayer.clientWorld.playSound(source.x, source.y, source.z, SoundEvents.ENTITY_VILLAGER_AMBIENT, SoundCategory.PLAYERS, 0.5f, 1f, true);
+            villagerCooldown = 300;
+        }
+
+        // hallucinate allay nosies and amethyst chimes
+        if (cPlayer.hasStatusEffect(Hierophantics.MEDIA_DISCOUNT_EFFECT.get())) {
+            if (allayCooldown > 0) {
+                allayCooldown--;
+            } else if (RAND.nextDouble() < 0.004) {
+                Vec3d source = randomSpherePoint(RAND.nextDouble(3.5, 6)).add(cPlayer.getPos());
+                if (RAND.nextDouble() < 0.5) {
+                    cPlayer.clientWorld.playSound(source.x, source.y, source.z, SoundEvents.BLOCK_AMETHYST_BLOCK_CHIME, SoundCategory.PLAYERS, 1f, (0.5f + RAND.nextFloat() * 1.2F), true);
+                } else {
+                    cPlayer.clientWorld.playSound(source.x, source.y, source.z, SoundEvents.ENTITY_ALLAY_AMBIENT_WITHOUT_ITEM, SoundCategory.PLAYERS, 0.3f, 1f, true);
+                }
+                allayCooldown = 70;
+            }
+        }
+    }
+
+    private Vec3d randomSpherePoint(double radius) {
+        double x = RAND.nextGaussian();
+        double y = RAND.nextGaussian();
+        double z = RAND.nextGaussian();
+        return new Vec3d(x, y, z).normalize().multiply(radius);
+    }
+}
