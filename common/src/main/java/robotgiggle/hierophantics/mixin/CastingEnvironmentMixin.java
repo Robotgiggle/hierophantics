618ae61c53553254aaceced745dff7c456681dde
Robotgiggle <purplemrb12@gmail.com>
2025-09-02 14:19:33 -0400

    Only apply media discount to player-based casting (resolves #11)

--- common/src/main/java/robotgiggle/hierophantics/mixin/CastingEnvironmentMixin.java
+++ common/src/main/java/robotgiggle/hierophantics/mixin/CastingEnvironmentMixin.java
@@ -2,6 +2,7 @@ package robotgiggle.hierophantics.mixin;
 
 import net.minecraft.entity.LivingEntity;
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment;
+import at.petrak.hexcasting.api.casting.eval.env.PlayerBasedCastEnv;
 import robotgiggle.hierophantics.Hierophantics;
 import robotgiggle.hierophantics.inits.HierophanticsConfig;
 
@@ -11,12 +12,13 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(value = CastingEnvironment.class, remap = false)
 public class CastingEnvironmentMixin {
-    // once hexcasting releases an update, I can use the new MEDIA_CONSUMPTION_MODIFIER attribute
-    // but for now I just replicate the effect with a mixin
     @ModifyVariable(method = "extractMedia", at = @At("HEAD"), argsOnly = true)
     private long applyMediaDiscount(long original) {
         CastingEnvironment env = (CastingEnvironment) (Object) this;
-        if (env.getCastingEntity() != null && env.getCastingEntity().hasStatusEffect(Hierophantics.MEDIA_DISCOUNT_EFFECT.get())) {
+        if (env instanceof PlayerBasedCastEnv 
+         && env.getCastingEntity() != null 
+         && env.getCastingEntity().hasStatusEffect(Hierophantics.MEDIA_DISCOUNT_EFFECT.get())
+        ) {
             return (long)Math.ceil(HierophanticsConfig.getServer().getMediaDiscount() * (double)original);
         }
         return original;
